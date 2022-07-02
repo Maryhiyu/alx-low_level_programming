ee9dfed085d8a6009e784d68950e3c562a8be670
maryhiyu <maryshu2j@gmail.com>
2022-07-01 18:10:58 -0700

    5print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ */
+int main(void)
+{
+	int Z;
+	for (Z = 'z'; Z >= 'a'; Z-)
+	{
+		putchar(Z);
+	}
+	putchar('\n');
+	return (0);
+}
+/* q7 */
