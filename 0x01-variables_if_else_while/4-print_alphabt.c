d75662d13c7cd4e873411e31dc657b03bbb33f73
maryhiyu <maryshu2j@gmail.com>
2022-07-01 18:01:59 -0700

    4print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int Z;
+	for (Z = 'z'; Z >= 'a'; Z-)
+	{
+		putchar(Z);
+	}
+	putchar('\n');
+	return (0);
+}
+/* q7 */
